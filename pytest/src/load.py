f5401415cde443188321c5e8da785107a7d0b13f
asiazhang <asiazhang2002@gmail.com>
2024-04-17 20:07:01 +0800

    entry文件使用绝对路径加载

--- pytest/src/load.py
+++ pytest/src/load.py
@@ -5,12 +5,16 @@ from typing import Optional, List, BinaryIO
 from dacite import from_dict
 from testsolar_testtool_sdk.model.param import EntryParam
 
-from .pytestx.collector import collect_testcases
+from pathlib import Path
+parent = Path(__file__).parent.resolve().joinpath('pytestx')
+sys.path.append(str(parent))
 
 
 def collect_testcases_from_args(
-    args: List[str], workspace: Optional[str] = None, pipe_io: Optional[BinaryIO] = None
+        args: List[str], workspace: Optional[str] = None, pipe_io: Optional[BinaryIO] = None
 ) -> None:
+    from pytestx.collector import collect_testcases
+
     if len(args) != 2:
         raise SystemExit("Usage: python load.py <entry_file>")
 
--- pytest/src/run.py
+++ pytest/src/run.py
@@ -5,12 +5,17 @@ from typing import Optional, List, BinaryIO
 from dacite import from_dict
 from testsolar_testtool_sdk.model.param import EntryParam
 
-from .pytestx.executor import run_testcases
+from pathlib import Path
+
+parent = Path(__file__).parent.resolve().joinpath('pytestx')
+sys.path.append(str(parent))
 
 
 def run_testcases_from_args(
-    args: List[str], workspace: Optional[str] = None, pipe_io: Optional[BinaryIO] = None
+        args: List[str], workspace: Optional[str] = None, pipe_io: Optional[BinaryIO] = None
 ) -> None:
+    from pytestx.executor import run_testcases
+
     if len(args) != 2:
         raise SystemExit("Usage: python run.py <entry_file>")
 
