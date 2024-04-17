db49a335e7756aa698213843a1b685ade24edf2b
asiazhang <asiazhang2002@gmail.com>
2024-04-17 18:51:33 +0800

    修复pdm的运行路径

--- pytest/script/linux/install.sh
+++ pytest/script/linux/install.sh
@@ -10,7 +10,7 @@ cd "${TOOL_ROOT}"
 curl -sSL https://pdm-project.org/install-pdm.py | python3 -
 
 # 安装到全局的python中，无需创建虚拟环境(当前已经使用uniSDK，对运行环境的依赖很少)
-pdm install -g
+"$HOME"/.local/bin/pdm install -g
 
 # 将version信息写进文件，来判断走sdk V1还是V2逻辑
 solarctl version > /tmp/solarctl_version
