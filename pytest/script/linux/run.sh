a275ea98268bf9f671cd4bf0dde26d9463fac72d
asiazhang <asiazhang2002@gmail.com>
2024-04-24 19:41:53 +0800

    更新uniSDK版本

--- pytest/script/linux/run.sh
+++ pytest/script/linux/run.sh
@@ -2,10 +2,6 @@
 
 set -exu -o pipefail
 
-# 暂时仅支持amd64，后续再支持arm64
-curl -Lk https://github.com/OpenTestSolar/testtools-uni-sdk/releases/download/0.0.1/testtools_sdk.amd64 -o /usr/local/bin/testtools_sdk
-chmod +x /usr/local/bin/testtools_sdk
-
 TOOL_ROOT=$(realpath "$0" | xargs dirname | xargs dirname | xargs dirname)
 echo "${TOOL_ROOT}"
 echo "$TESTSOLAR_WORKSPACE"
