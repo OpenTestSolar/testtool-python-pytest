0a2a64858c6d73fc23e6ef2b3188e71e22fda005
ambzhang <ambzhang@tencent.com>
2024-04-26 16:16:40 +0800

    add test

--- pytest/tests/test_run_allure.py
+++ pytest/tests/test_run_allure.py
@@ -15,14 +15,27 @@ class TestExecuteEntry(TestCase):
     )
 
     def test_run_testcases_from_args(self):
-        os.environ["TESTSOLAR_TTP_ENABLEALLURE"] = "1" 
+        os.environ["TESTSOLAR_TTP_ENABLEALLURE"] = "1"
         pipe_io = io.BytesIO()
         run_testcases_from_args(
-            args=["run.py", Path.joinpath(Path(self.testdata_dir), "allure_entry.json")],
+            args=[
+                "run.py",
+                Path.joinpath(Path(self.testdata_dir), "allure_entry.json"),
+            ],
             workspace=self.testdata_dir,
             pipe_io=pipe_io,
         )
 
+        # testcase running
         pipe_io.seek(0)
         start = read_test_result(pipe_io)
         self.assertEqual(start.ResultType, ResultType.RUNNING)
+        self.assertEqual(start.Test.Name, "allure/allure_step_test.py?test_step")
+
+        # testcase finish
+        stop = read_test_result(pipe_io)
+        self.assertEqual(stop.ResultType, ResultType.SUCCEED)
+        self.assertEqual(stop.Test.Name, "allure/allure_step_test.py?test_step")
+        self.assertEqual(stop.Message, "")
+        self.assertEqual(type(stop.Steps), list)
+        self.assertEqual(len(stop.Steps), 5)
