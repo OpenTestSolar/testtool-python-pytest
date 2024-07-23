e462b0e9d267ac5f900662b9f15e9de382e59a6d
asiazhang <asiazhang2002@gmail.com>
2024-07-23 18:58:23 +0800

    Feature use pdm requirements (#67)
    * 使用pdm export的requirements.txt
    
    * bump version

--- pytest/script/linux/install.sh
+++ pytest/script/linux/install.sh
@@ -8,7 +8,7 @@ echo "${TOOL_ROOT}"
 cd "${TOOL_ROOT}"
 
 # 安装到全局的python中，无需创建虚拟环境(当前已经使用uniSDK，对运行环境的依赖很少)
-pip3 install "pytest>=7" "pytest-timeout>=2.3" "testsolar-testtool-sdk>=0.2.1"
+pip3 install -r requirements.txt
 
 # 将version信息写进文件，来判断走sdk V1还是V2逻辑
 solarctl version > /tmp/solarctl_version
