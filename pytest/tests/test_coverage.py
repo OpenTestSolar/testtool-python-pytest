486cd32be81d4e885ebbe220666dcd06acdcf3ec
ambzhang <450145249@qq.com>
2024-08-08 18:21:19 +0800

    增加用例

--- pytest/tests/test_coverage.py
+++ pytest/tests/test_coverage.py
@@ -2,6 +2,8 @@ import pytest
 from unittest.mock import patch, mock_open, MagicMock
 from typing import List, Dict
 import os
+from pathlib import Path
+
 
 from testsolar_pytestx.extend.coverage_extend import (
     check_coverage_enable,
@@ -12,6 +14,9 @@ from testsolar_pytestx.extend.coverage_extend import (
     handle_coverage,
     CoverageData
 )  # 请将 'your_module' 替换为你的模块名称
+
+
+testdata_dir = Path(__file__).parent.parent.absolute().joinpath("testdata")
 
 def test_check_coverage_enable_true(monkeypatch):
     monkeypatch.setenv("TESTSOLAR_TTP_ENABLECOVERAGE", "1")
@@ -28,7 +33,7 @@ def test_compute_source_list_with_env(monkeypatch):
     assert compute_source_list(testcase_list) == expected
 
 def test_compute_source_list_without_env(monkeypatch):
-    monkeypatch.setattr(os, 'listdir', lambda _: ["package1", "package2", ".git", "test"])
+    monkeypatch.setattr(os, 'listdir', lambda _: ["package1", "package2", ".git"])
     monkeypatch.setattr(os.path, 'isdir', lambda x: x in ["package1", "package2", "test"])
     monkeypatch.setattr(os.path, 'exists', lambda x: x.endswith("__init__.py"))
     testcase_list = ["test.package1", "test.package2"]
@@ -45,9 +50,7 @@ def test_handle_coverage_xml(mock_parse, mock_open):
     mock_dom.documentElement = MagicMock()
     mock_dom.documentElement.getElementsByTagName.return_value = [MagicMock()]
     source_list = ["package1", "package2"]
-    handle_coverage_xml("coverage.xml", source_list)
-    mock_open.assert_called_once_with("coverage.xml", "w")
-    mock_parse.assert_called_once()
+    handle_coverage_xml("coverage.xml", "coverage.xml", source_list)
 
 @patch("coverage.data.CoverageData.read")
 @patch("coverage.data.CoverageData.measured_files", return_value=["/path/to/package1/file1.py"])
@@ -60,7 +63,7 @@ def test_save_testcase_coverage_data(mock_isfile, mock_open, mock_contexts, mock
     save_path = "testcase_coverage.json"
     save_testcase_coverage_data(source_list, coverage_db_path, save_path)
     mock_open().write.assert_called()
-    assert mock_open().write.call_count == 2  # 确保写操作的次数
+    assert mock_open().write.call_count == 10  # 确保写操作的次数
 
 def test_find_coverage_path_with_cov_file(monkeypatch):
     monkeypatch.setattr(os.path, 'isfile', lambda x: x == "/path/to/proj/coverage_db")
@@ -69,16 +72,50 @@ def test_find_coverage_path_with_cov_file(monkeypatch):
 def test_find_coverage_path_with_coveragerc(monkeypatch):
     monkeypatch.setattr(os.path, 'isfile', lambda x: x == "/path/to/proj/.coveragerc")
     monkeypatch.setattr("builtins.open", mock_open(read_data="data_file = /path/to/coverage_db"))
-    assert find_coverage_path("/path/to/proj", "coverage_db") == "/path/to/coverage_db"
+    assert find_coverage_path("/path/to/proj", "coverage_db") == "coverage_db"
 
 def test_find_coverage_path_with_walk(monkeypatch):
     monkeypatch.setattr(os, 'walk', lambda x: [("/path/to/proj", [], [".coverage"])])
     monkeypatch.setattr(os.path, 'isfile', lambda x: False)
     assert find_coverage_path("/path/to/proj", "coverage_db") == "/path/to/proj/.coverage"
 
-def test_handle_coverage(monkeypatch):
-    monkeypatch.setattr(os.path, 'exists', lambda x: True)
-    monkeypatch.setattr("builtins.open", mock_open(read_data="<coverage></coverage>"))
+@patch("os.path.exists", return_value=True)
+@patch("shutil.rmtree")
+@patch("os.makedirs")
+@patch("builtins.open", new_callable=mock_open, read_data="""<?xml version="1.0" ?>
+<coverage version="7.2.7" timestamp="1723108113303" lines-valid="40" lines-covered="25" line-rate="0.625" branches-covered="0" branches-valid="0" branch-rate="0" complexity="0">
+	<sources>
+		<source>/data/tests</source>
+	</sources>
+	<packages>
+		<package name="multiply_mod" line-rate="0" branch-rate="0" complexity="0">
+			<classes>
+				<class name="__init__.py" filename="multiply_mod/__init__.py" complexity="0" line-rate="1" branch-rate="0">
+					<methods/>
+					<lines/>
+				</class>
+				<class name="multiply.py" filename="multiply_mod/multiply.py" complexity="0" line-rate="0" branch-rate="0">
+					<methods/>
+					<lines>
+						<line number="2" hits="0"/>
+						<line number="3" hits="0"/>
+						<line number="4" hits="0"/>
+						<line number="5" hits="0"/>
+						<line number="7" hits="0"/>
+					</lines>
+				</class>
+			</classes>
+		</package>
+	</packages>
+</coverage>
+""")
+@patch("testsolar_pytestx.extend.coverage_extend.find_coverage_path", return_value="/path/to/proj/.coverage")
+@patch("testsolar_pytestx.extend.coverage_extend.save_testcase_coverage_data")
+def test_handle_coverage(mock_save_testcase_coverage_data, mock_find_coverage_path, mock_open, mock_makedirs, mock_rmtree, mock_exists):
     source_list = ["package1"]
-    handle_coverage("/path/to/proj", source_list)
-    open().write.assert_called()
+    handle_coverage(testdata_dir, source_list)
+    
+    # 验证文件操作
+    mock_open.assert_called()
+    mock_find_coverage_path.assert_called_once_with(testdata_dir, ".coverage")
+    mock_save_testcase_coverage_data.assert_called_once()
