0c0f6bf1484d9b3f0299b0d9c184553eb8a8078d
ambzhang <450145249@qq.com>
2024-08-09 15:24:37 +0800

    format

--- pytest/src/testsolar_pytestx/extend/coverage_extend.py
+++ pytest/src/testsolar_pytestx/extend/coverage_extend.py
@@ -10,21 +10,25 @@ import coverage
 
 COVERAGE_DIR: str = "testsolar_coverage"
 
+
 @dataclass
 class TestFileLines:
     fileName: str
     fileLines: List[int]
+
 
 @dataclass
 class TestCaseCoverage:
     caseName: str
     testFiles: List[TestFileLines] = field(default_factory=list)
 
+
 @dataclass
 class ProjectPath:
     projectPath: str
     beforeMove: str = ""
     afterMove: str = ""
+
 
 @dataclass
 class Coverage:
@@ -38,13 +42,16 @@ class Coverage:
     def to_json(self) -> str:
         return json.dumps(asdict(self), indent=4)
 
+
 @dataclass
 class CoverageData:
     name: str
     files: Dict[str, List[int]] = field(default_factory=dict)
 
+
 def check_coverage_enable() -> bool:
     return os.getenv("TESTSOLAR_TTP_ENABLECOVERAGE", "") in ["1", "true"]
+
 
 def compute_source_list(testcase_list: List[str]) -> List[str]:
     source_list_env: str = os.environ.get("TESTSOLAR_TTP_COVERAGESOURCELIST", "")
@@ -52,12 +59,12 @@ def compute_source_list(testcase_list: List[str]) -> List[str]:
         source_list: List[str] = source_list_env.strip().split(";")
     else:
         source_list = []
-        
+
     testcase_package_list: List[str] = []
     for it in testcase_list:
         testcase_package_list.append(it.split("/")[0].strip())
     testcase_package_list = list(set(testcase_package_list))
-    
+
     if not source_list:
         for it in os.listdir(os.getcwd()):
             if not os.path.isdir(it):
@@ -71,6 +78,7 @@ def compute_source_list(testcase_list: List[str]) -> List[str]:
             source_list.append(it)
 
     return source_list
+
 
 def handle_coverage_xml(xml_path: str, source_list: List[str]) -> None:
     start_time: float = time.time()
@@ -94,37 +102,31 @@ def handle_coverage_xml(xml_path: str, source_list: List[str]) -> None:
             dom.writexml(fd)
     print("handle_coverage_xml cost time: %s" % (time.time() - start_time))
 
-def get_testcase_coverage_data(source_list: List[str], coverage_db_path: str) -> Dict[str, CoverageData]:
+
+def get_testcase_coverage_data(
+    source_list: List[str], coverage_db_path: str
+) -> Dict[str, CoverageData]:
     if not os.path.isfile(coverage_db_path):
         raise RuntimeError(f"Coverage db {coverage_db_path} not exist")
     root_path: str = os.path.dirname(os.path.abspath(coverage_db_path))
     result: Dict[str, CoverageData] = {}
-    source_list = ['multiply_mod', 'addition_mod']
+    source_list = ["multiply_mod", "addition_mod"]
     cov = coverage.Coverage(data_file=coverage_db_path)
     cov.load()
     file_set = cov.get_data().measured_files()
     print("so")
     for fn in file_set:
-        print("====111", fn)
-
         rav_fn = fn
-        print("====1.5", rav_fn)
-        print("====1.6", root_path)
         if rav_fn.startswith(root_path):
-            print("====1.7")
             rav_fn = rav_fn[len(root_path) + 1 :]
         for source in source_list:
-            print("====1.8", source)
             if rav_fn.startswith(source):
-                print("=====1.9")
                 break
         else:
-            print("====333")
             continue
 
         line_map = cov.get_data().lines(fn)
         context_map = cov.get_data().contexts_by_lineno(fn)
-        print("====222", line_map)
         if line_map is not None:
             for line in line_map:
                 test_case_list = context_map.get(line, [])
@@ -147,6 +149,7 @@ def get_testcase_coverage_data(source_list: List[str], coverage_db_path: str) ->
 
     return result
 
+
 def find_coverage_path(proj: str, cov_file: str) -> str:
     if os.path.isfile(os.path.join(proj, cov_file)):
         print("find coverage db file: ", cov_file)
@@ -168,36 +171,38 @@ def find_coverage_path(proj: str, cov_file: str) -> str:
                 return os.path.join(root, f_name)
     return cov_file
 
+
 def ensure_directory_exists_and_clear(directory: str) -> None:
     try:
         if os.path.exists(directory):
             shutil.rmtree(directory)
             print(f"Directory '{directory}' was cleared.")
-        
+
         os.makedirs(directory)
         print(f"Directory '{directory}' is ready.")
-        
+
     except OSError as e:
         print(f"Error: {e.strerror}")
+
 
 def handle_coverage(proj_path: str, source_list: List[str]) -> None:
     ensure_directory_exists_and_clear(os.path.join(proj_path, COVERAGE_DIR))
     coverage_file_path: str = os.path.join(proj_path, "coverage.xml")
-    coverage_json_file: str = os.path.join(proj_path, COVERAGE_DIR, "testsolar_coverage.json")
-    
+    coverage_json_file: str = os.path.join(
+        proj_path, COVERAGE_DIR, "testsolar_coverage.json"
+    )
+
     if not os.path.exists(coverage_file_path):
         print("File coverage.xml not exist", file=sys.stderr)
         return
-    
+
     print("handle coverage.xml")
     handle_coverage_xml(coverage_file_path, source_list)
     coverage_db_path = find_coverage_path(proj_path, ".coverage")
     cov_file_info = get_testcase_coverage_data(source_list, coverage_db_path)
 
     project_path = ProjectPath(
-        projectPath=str(proj_path),  # 确保转换为字符串
-        beforeMove="",
-        afterMove=""
+        projectPath=str(proj_path), beforeMove="", afterMove=""  # 确保转换为字符串
     )
 
     coverage_data = Coverage(
@@ -205,13 +210,13 @@ def handle_coverage(proj_path: str, source_list: List[str]) -> None:
         coverageType="cobertura_xml",
         reportId=os.getenv("QTA_REPORTID", ""),
         resultHouseReportId=os.getenv("QTA_RESULT_HOUSE_REPORTID", ""),
-        projectPath=project_path
+        projectPath=project_path,
     )
-    print("=====123123123", cov_file_info)
     for case_name, file_covs in cov_file_info.items():
-
-        test_files = [TestFileLines(fileName=file_name, fileLines=file_lines)
-                      for file_name, file_lines in file_covs.files.items()]
+        test_files = [
+            TestFileLines(fileName=file_name, fileLines=file_lines)
+            for file_name, file_lines in file_covs.files.items()
+        ]
         test_case_coverage = TestCaseCoverage(caseName=case_name, testFiles=test_files)
         coverage_data.caseCoverage.append(test_case_coverage)
 
@@ -219,9 +224,3 @@ def handle_coverage(proj_path: str, source_list: List[str]) -> None:
         f.write(coverage_data.to_json())
 
     print(f"Coverage data saved to {coverage_json_file}")
-
-
-if __name__ == "__main__":
-    testcase_list: List[str] = ["uttest/test_add.py?test_add_2_numbers", "uttest/test_add.py?test_add_dict"]
-    source_list = compute_source_list(testcase_list)
-    handle_coverage(os.getcwd(), source_list)
--- pytest/tests/test_coverage.py
+++ pytest/tests/test_coverage.py
@@ -12,19 +12,22 @@ from testsolar_pytestx.extend.coverage_extend import (
     get_testcase_coverage_data,
     find_coverage_path,
     handle_coverage,
-    CoverageData
+    CoverageData,
 )  # 请将 'your_module' 替换为你的模块名称
 
 
 testdata_dir = Path(__file__).parent.parent.absolute().joinpath("testdata")
 
+
 def test_check_coverage_enable_true(monkeypatch):
     monkeypatch.setenv("TESTSOLAR_TTP_ENABLECOVERAGE", "1")
     assert check_coverage_enable() is True
 
+
 def test_check_coverage_enable_false(monkeypatch):
     monkeypatch.setenv("TESTSOLAR_TTP_ENABLECOVERAGE", "0")
     assert check_coverage_enable() is False
+
 
 def test_compute_source_list_with_env(monkeypatch):
     monkeypatch.setenv("TESTSOLAR_TTP_COVERAGESOURCELIST", "package1;package2")
@@ -32,15 +35,19 @@ def test_compute_source_list_with_env(monkeypatch):
     expected = ["package1", "package2"]
     assert compute_source_list(testcase_list) == expected
 
+
 def test_compute_source_list_without_env(monkeypatch):
-    monkeypatch.setattr(os, 'listdir', lambda _: ["package1", "package2", ".git"])
-    monkeypatch.setattr(os.path, 'isdir', lambda x: x in ["package1", "package2", "test"])
-    monkeypatch.setattr(os.path, 'exists', lambda x: x.endswith("__init__.py"))
+    monkeypatch.setattr(os, "listdir", lambda _: ["package1", "package2", ".git"])
+    monkeypatch.setattr(
+        os.path, "isdir", lambda x: x in ["package1", "package2", "test"]
+    )
+    monkeypatch.setattr(os.path, "exists", lambda x: x.endswith("__init__.py"))
     testcase_list = ["test.package1", "test.package2"]
     expected = ["package1", "package2"]
     result = compute_source_list(testcase_list)
     assert "test" not in result
     assert set(result) == set(expected)
+
 
 @patch("builtins.open", new_callable=mock_open, read_data="<coverage></coverage>")
 @patch("xml.dom.minidom.parse")
@@ -52,60 +59,76 @@ def test_handle_coverage_xml(mock_parse, mock_open):
     source_list = ["package1", "package2"]
     handle_coverage_xml("coverage.xml", source_list)
 
+
 @patch("coverage.data.CoverageData.read")
-@patch("coverage.data.CoverageData.measured_files", return_value=["/path/to/package1/file1.py"])
-@patch("coverage.data.CoverageData.contexts_by_lineno", return_value={10: ["test.package1::test_func|context"]})
+@patch(
+    "coverage.data.CoverageData.measured_files",
+    return_value=["/path/to/package1/file1.py"],
+)
+@patch(
+    "coverage.data.CoverageData.contexts_by_lineno",
+    return_value={10: ["test.package1::test_func|context"]},
+)
 @patch("builtins.open", new_callable=mock_open)
 @patch("os.path.isfile", return_value=True)
-def test_get_testcase_coverage_data(mock_isfile, mock_open, mock_contexts, mock_files, mock_read):
+def test_get_testcase_coverage_data(
+    mock_isfile, mock_open, mock_contexts, mock_files, mock_read
+):
     source_list = ["package1"]
     coverage_db_path = "/tmp/to/coverage_db"
     get_testcase_coverage_data(source_list, coverage_db_path)
 
 
 def test_find_coverage_path_with_cov_file(monkeypatch):
-    monkeypatch.setattr(os.path, 'isfile', lambda x: x == "/path/to/proj/coverage_db")
+    monkeypatch.setattr(os.path, "isfile", lambda x: x == "/path/to/proj/coverage_db")
     assert find_coverage_path("/path/to/proj", "coverage_db") == "coverage_db"
+
 
 def test_find_coverage_path_with_coveragerc(monkeypatch):
-    monkeypatch.setattr(os.path, 'isfile', lambda x: x == "/path/to/proj/.coveragerc")
-    monkeypatch.setattr("builtins.open", mock_open(read_data="data_file = /path/to/coverage_db"))
+    monkeypatch.setattr(os.path, "isfile", lambda x: x == "/path/to/proj/.coveragerc")
+    monkeypatch.setattr(
+        "builtins.open", mock_open(read_data="data_file = /path/to/coverage_db")
+    )
     assert find_coverage_path("/path/to/proj", "coverage_db") == "coverage_db"
 
+
 def test_find_coverage_path_with_walk(monkeypatch):
-    monkeypatch.setattr(os, 'walk', lambda x: [("/path/to/proj", [], [".coverage"])])
-    monkeypatch.setattr(os.path, 'isfile', lambda x: False)
-    assert find_coverage_path("/path/to/proj", "coverage_db") == "/path/to/proj/.coverage"
+    monkeypatch.setattr(os, "walk", lambda x: [("/path/to/proj", [], [".coverage"])])
+    monkeypatch.setattr(os.path, "isfile", lambda x: False)
+    assert (
+        find_coverage_path("/path/to/proj", "coverage_db") == "/path/to/proj/.coverage"
+    )
+
 
 @patch("os.path.exists", return_value=True)
 @patch("shutil.rmtree")
 @patch("os.makedirs")
-@patch("testsolar_pytestx.extend.coverage_extend.find_coverage_path", return_value="/path/to/proj/.coverage")
-@patch("testsolar_pytestx.extend.coverage_extend.get_testcase_coverage_data", return_value={
-  "uttest.test_add.test_add_2_numbers": 
-  CoverageData(
-      name="uttest.test_add.test_add_2_numbers", 
-      files={
-            "addition_mod/add.py": [
-            11,
-            12
-            ]
-        }
-    ),
-  "uttest.test_add.test_add_param": 
-  CoverageData(
-      name="uttest.test_add.test_add_param", 
-      files={
-            "addition_mod/add.py": [
-            11,
-            12
-            ]
-        }
-    )
-})
-def test_handle_coverage(mock_get_testcase_coverage_data, mock_find_coverage_path, mock_makedirs, mock_rmtree, mock_exists):
+@patch(
+    "testsolar_pytestx.extend.coverage_extend.find_coverage_path",
+    return_value="/path/to/proj/.coverage",
+)
+@patch(
+    "testsolar_pytestx.extend.coverage_extend.get_testcase_coverage_data",
+    return_value={
+        "uttest.test_add.test_add_2_numbers": CoverageData(
+            name="uttest.test_add.test_add_2_numbers",
+            files={"addition_mod/add.py": [11, 12]},
+        ),
+        "uttest.test_add.test_add_param": CoverageData(
+            name="uttest.test_add.test_add_param",
+            files={"addition_mod/add.py": [11, 12]},
+        ),
+    },
+)
+def test_handle_coverage(
+    mock_get_testcase_coverage_data,
+    mock_find_coverage_path,
+    mock_makedirs,
+    mock_rmtree,
+    mock_exists,
+):
     source_list = ["package1"]
-    
+
     # 运行 handle_coverage 函数
     handle_coverage(testdata_dir, source_list)
 
@@ -117,11 +140,16 @@ def test_handle_coverage(mock_get_testcase_coverage_data, mock_find_coverage_pat
     coverage_json_file = testdata_dir / "testsolar_coverage" / "testsolar_coverage.json"
     with open(coverage_json_file, "r") as f:
         written_data = json.load(f)
-    
+
     assert written_data["coverageFile"] == str(testdata_dir / "coverage.xml")
     assert written_data["coverageType"] == "cobertura_xml"
     assert written_data["projectPath"]["projectPath"] == str(testdata_dir)
     assert "caseCoverage" in written_data
     assert len(written_data["caseCoverage"]) == 2
-    assert written_data["caseCoverage"][0]["caseName"] == "uttest.test_add.test_add_2_numbers"
-    assert written_data["caseCoverage"][1]["caseName"] == "uttest.test_add.test_add_param"
+    assert (
+        written_data["caseCoverage"][0]["caseName"]
+        == "uttest.test_add.test_add_2_numbers"
+    )
+    assert (
+        written_data["caseCoverage"][1]["caseName"] == "uttest.test_add.test_add_param"
+    )
